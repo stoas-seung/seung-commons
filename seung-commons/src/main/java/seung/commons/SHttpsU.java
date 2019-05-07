87be9a3c57c75c1ebd23bb7e68dc858698647d94
Park Jong Seung <stoas.seung@gmail.com>
2019-05-07 14:05:12 +0900

    add SHttpsU.requestWithFile

--- seung-commons/src/main/java/seung/commons/SHttpsU.java
+++ seung-commons/src/main/java/seung/commons/SHttpsU.java
@@ -1,12 +1,11 @@
 package seung.commons;
 
 import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -20,6 +19,7 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 import seung.commons.http.SHttpVO;
@@ -122,102 +122,107 @@ public class SHttpsU {
 		return sHttpVO;
 	}
 	
-//	public static SHttpVO requestWithFile(SHttpVO sHttpVO, File[] files) {
-//		
-//		String             boundary           = "----" + SCommonU.getUUID();
-//		
-//		boolean            isPost             = SCommonV._S_METHOD_POST.equals(sHttpVO.getRequestMethod()) ? true : false;
-//		HttpsURLConnection httpsURLConnection = null;
-//		BufferedWriter     bufferedWriter     = null;
-//		OutputStream       outputStream       = null;
-//		InputStreamReader  inputStreamReader  = null;
-//		
-//		try {
-//			
-//			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-//				@Override
-//				public boolean verify(String hostname, SSLSession session) {
-//					return true;
-//				}
-//			});
-//			
-//			SSLContext sslContext = SSLContext.getInstance("SSL");
-//			sslContext.init(
-//					null
-//					, new TrustManager[] {
-//						new X509TrustManager() {
-//							@Override
-//							public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-//								return null;
-//							}
-//							@Override
-//							public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-//								
-//							}
-//							@Override
-//							public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
-//							}
-//						}
-//					}
-//					, new SecureRandom()
-//					);
-//			HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
-//			
-//			URL url = new URL(sHttpVO.getRequestUrl());
-//			httpsURLConnection = (HttpsURLConnection) url.openConnection();
-//			
-//			if(sHttpVO.getRequestHeaders() != null) {
-//				for(String[] header : sHttpVO.getRequestHeaders()) {
-//					httpsURLConnection.setRequestProperty(header[0], header[1]);
-//				}
-//			}
-//			
-//			httpsURLConnection.setRequestMethod(sHttpVO.getRequestMethod());
-//			httpsURLConnection.setConnectTimeout(sHttpVO.getConnectionTimeout());
-//			httpsURLConnection.setReadTimeout(sHttpVO.getReadTimeout());
-//			httpsURLConnection.setUseCaches(false);
-//			httpsURLConnection.setDoInput(true);
-//			httpsURLConnection.setDoOutput(true);
-//			httpsURLConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-//			
-//			outputStream = new BufferedOutputStream(httpsURLConnection.getOutputStream());
-//			bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-//			if(isPost) {
-//				outputStream.write(sHttpVO.getQueryString().getBytes(sHttpVO.getRequestEncoding()));
-//			}
-//			outputStream.flush();
-//			
-//			sHttpVO.setResponseCode(httpsURLConnection.getResponseCode());
-//			sHttpVO.setContentType(httpsURLConnection.getContentType());
-//			sHttpVO.setContentLength(httpsURLConnection.getContentLength());
-//			sHttpVO.setContentDisposition(httpsURLConnection.getHeaderField("Content-Disposition"));
-//			
-//			inputStreamReader = new InputStreamReader(httpsURLConnection.getInputStream());
-//			sHttpVO.setResponse(IOUtils.toByteArray(inputStreamReader, sHttpVO.getResponseEncoding()));
-//			
-//		} catch (NoSuchAlgorithmException e) {
-//			sHttpVO.setExceptionMessage(e.getMessage());
-//		} catch (KeyManagementException e) {
-//			sHttpVO.setExceptionMessage(e.getMessage());
-//		} catch (IOException e) {
-//			sHttpVO.setExceptionMessage(e.getMessage());
-//		} finally {
-//			try {
-//				if(inputStreamReader != null) {
-//					inputStreamReader.close();
-//				}
-//				if(outputStream != null) {
-//					outputStream.close();
-//				}
-//				if(httpsURLConnection != null) {
-//					httpsURLConnection.disconnect();
-//				}
-//			} catch (IOException e) {
-//				sHttpVO.setExceptionMessage(e.getMessage());
-//			}
-//		}
-//		
-//		return sHttpVO;
-//	}
+	public static SHttpVO requestWithFile(SHttpVO sHttpVO, String fileFieldName, File file) {
+		
+		String             boundary           = "----" + SCommonU.getUUID();
+		String             delim              = "--";
+		String             newLine            = "\r\n";
+		
+		HttpsURLConnection httpsURLConnection = null;
+		DataOutputStream   dataOutputStream   = null;
+		InputStreamReader  inputStreamReader  = null;
+		
+		try {
+			
+			HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+				@Override
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			});
+			
+			SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(
+					null
+					, new TrustManager[] {
+						new X509TrustManager() {
+							@Override
+							public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+								return null;
+							}
+							@Override
+							public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+								
+							}
+							@Override
+							public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+							}
+						}
+					}
+					, new SecureRandom()
+					);
+			HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+			
+			URL url = new URL(sHttpVO.getRequestUrl());
+			httpsURLConnection = (HttpsURLConnection) url.openConnection();
+			
+			if(sHttpVO.getRequestHeaders() != null) {
+				for(String[] header : sHttpVO.getRequestHeaders()) {
+					httpsURLConnection.setRequestProperty(header[0], header[1]);
+				}
+			}
+			
+			httpsURLConnection.setRequestMethod(sHttpVO.getRequestMethod());
+			httpsURLConnection.setConnectTimeout(sHttpVO.getConnectionTimeout());
+			httpsURLConnection.setReadTimeout(sHttpVO.getReadTimeout());
+			httpsURLConnection.setUseCaches(false);
+			httpsURLConnection.setDoInput(true);
+			httpsURLConnection.setDoOutput(true);
+			httpsURLConnection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+			
+			dataOutputStream = new DataOutputStream(httpsURLConnection.getOutputStream());
+			dataOutputStream.writeBytes(delim + boundary + newLine);
+			dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + fileFieldName + "\"; filename=\"" + file.getName() + "\"" + newLine);
+			dataOutputStream.writeBytes("Content-Type: application/octet-stream" + newLine);
+			dataOutputStream.writeBytes(newLine);
+			dataOutputStream.write(FileUtils.readFileToByteArray(file));
+			dataOutputStream.writeBytes(newLine);
+			for(String[] requestParameters : sHttpVO.getRequestParameters()) {
+				dataOutputStream.writeBytes(delim + boundary + newLine);
+				dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"" + requestParameters[0] + "\"" + newLine);
+				dataOutputStream.writeBytes(newLine);
+				dataOutputStream.writeBytes(requestParameters[1]);
+				dataOutputStream.writeBytes(newLine);
+			}
+			dataOutputStream.writeBytes(delim + boundary + delim);
+			dataOutputStream.flush();
+			
+			inputStreamReader = new InputStreamReader(httpsURLConnection.getInputStream());
+			sHttpVO.setResponse(IOUtils.toByteArray(inputStreamReader, sHttpVO.getResponseEncoding()));
+			
+		} catch (NoSuchAlgorithmException e) {
+			sHttpVO.setExceptionMessage(e.getMessage());
+		} catch (KeyManagementException e) {
+			sHttpVO.setExceptionMessage(e.getMessage());
+		} catch (IOException e) {
+			sHttpVO.setExceptionMessage(e.getMessage());
+		} finally {
+			try {
+				if(inputStreamReader != null) {
+					inputStreamReader.close();
+				}
+				if(dataOutputStream != null) {
+					dataOutputStream.close();
+				}
+				if(httpsURLConnection != null) {
+					httpsURLConnection.disconnect();
+				}
+			} catch (IOException e) {
+				sHttpVO.setExceptionMessage(e.getMessage());
+			}
+		}
+		
+		return sHttpVO;
+	}
 	
 }
